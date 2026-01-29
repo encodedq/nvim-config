9b54728727ebd3048458d90a1393a1612d062262
encodedq <encodedq@gmail.com>
2026-01-29 23:18:18 +0200

    initial commit

---
+++ lua/config/init.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+
+require("config.lazy_init")
+require("config.set")
+require("config.remap")
+require("config.lazy.treesitter")
+require("config.lazy.telescope")
+require("config.lazy.colorscheme")
